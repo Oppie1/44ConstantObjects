684078bdb771ca269ff4bc515b6895f8878ab352
Oppie1 <atindall729@gmail.com>
2025-03-04 15:58:14 -0600

    Add files via upload

---
+++ 44ConstantObjects/44ConstantObjects.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Sally.h"
+using namespace std;
+
+int main() {
+
+	Sally salObj;
+	salObj.printShiz();
+
+	const Sally constObj;
+	constObj.printShiz2();
+}
---
+++ 44ConstantObjects/Sally.h
@@ -0,0 +1,14 @@
+#ifndef SALLY_H
+#define SALLY_H
+
+class Sally
+{
+public:
+	Sally();
+	void printShiz();
+	void printShiz2() const;
+protected:
+private:
+};
+
+#endif
